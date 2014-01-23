64d9f9a959ce76e6061acaaff4da9319b1edf18f
JMLjml <johnlasheski@gmail.com>
2014-01-22 22:02:00 -0500

    Added more tokens.

--- tokens.h
+++ tokens.h
@@ -1,1 +1,7 @@
-enum Tokens {RELOP = 256, ADDOP = 257, IDENT = 258};
+enum Tokens {RELOP = 256, ADDOP = 257, MULOP = 258, IDENT = 259, BEGIN_ = 260,
+ BOOLEAN = 261, ELSE = 262, END = 263, ENDIF = 264, FUNCTION = 265, IF = 266,
+ IS = 267, INT = 268, REAL = 269, BOOL = 270, RETURNS = 271, THEN = 272, AND = 273,
+ OR = 274, NOT = 275};
+
+ //BOOLEAN if for the keyword boolean
+ //BOOL is for the variabel type bool which has attribute of true or false
